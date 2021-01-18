5e626d4002b13e161e1e32267a9447d296d33576
Anatoly Tsybrov <ttoljke@gmail.com>
2021-01-19 01:17:23 +0300

    test failed

--- src/test/java/CardOrder.java
+++ src/test/java/CardOrder.java
@@ -14,5 +14,15 @@ public class CardOrder {
         $(".button__content").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+    @Test
+    void shouldTestNotCorrectName() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Igor Popov");
+        $("[data-test-id='phone'] input").setValue("+79875556565");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверн. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
 
 }
